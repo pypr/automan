6030bfebc9f4d3c057aea8e20efef078229eb444
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2017-08-26 00:03:12 +0530

    Adding several useful files.
    This code is taken out of the pysph repository since it does not need to
    be in PySPH.

---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+
+def get_version():
+    import os
+    data = {}
+    fname = os.path.join('automan', '__init__.py')
+    exec(compile(open(fname).read(), fname, 'exec'), data)
+    return data.get('__version__')
+
+
+install_requires = ['psutil']
+tests_require = ['pytest']
+
+classes = """
+Development Status :: 3 - Alpha
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: Education
+Intended Audience :: End Users/Desktop
+Intended Audience :: Science/Research
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Scientific/Engineering
+Topic :: Software Development :: Libraries
+Topic :: Utilities
+"""
+
+classifiers = [x.strip() for x in classes.splitlines() if x]
+
+setup(
+    name='automan',
+    version=get_version(),
+    author='Prabhu Ramachandran',
+    author_email='prabhu@aero.iitb.ac.in',
+    description='A simple Python-based automation framework.',
+    long_description=open('README.rst').read(),
+    license="BSD",
+    url='https://github.com/pypr/automan',
+    classifiers=classifiers,
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    package_dir={'automan': 'automan'},
+)
