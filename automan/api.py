efea84daaf7a6971525024c7149c43314750b69b
Abhinav Muta <mutaabhinav@gmail.com>
2018-09-01 19:44:33 +0530

    fixed pep8 errors

--- automan/api.py
+++ automan/api.py
@@ -1,12 +1,12 @@
-from .jobs import Job, Worker, LocalWorker, RemoteWorker, Scheduler # noqa
+from .jobs import Job, Worker, LocalWorker, RemoteWorker, Scheduler  # noqa
 
 from .automation import (  # noqa
     Automator, CommandTask, Problem, PySPHProblem, PySPHTask, RunAll,
     Simulation, SolveProblem, Task, TaskRunner, WrapperTask
 )
 
-from .automation import compare_runs, filter_by_name, filter_cases # noqa
+from .automation import compare_runs, filter_by_name, filter_cases  # noqa
 
-from .cluster_manager import ClusterManager # noqa
-from .conda_cluster_manager import CondaClusterManager # noqa
-from .edm_cluster_manager import CondaClusterManager # noqa
+from .cluster_manager import ClusterManager  # noqa
+from .conda_cluster_manager import CondaClusterManager  # noqa
+from .edm_cluster_manager import CondaClusterManager  # noqa
