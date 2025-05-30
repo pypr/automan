3925ae2c251e5e64d5f30127164255c098d8f050
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2025-05-30 22:38:44 +0530

    Fix readthedocs files.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'automan'
-copyright = '2018-2021, Prabhu Ramachandran'
+copyright = '2018-2025, Prabhu Ramachandran'
 author = 'Prabhu Ramachandran'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -75,7 +75,7 @@ version = release[:3]
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
